ac790373c08e7df8a8d2880f4718cbbd9322fa3d
Andres Torres <andres.torres@icogroup.com>
2015-01-09 14:42:43 -0430

    :star: Realizado diseño de pantalla Restaurant

--- app/src/main/java/com/icogroup/dallassuites/Restaurant.java
+++ app/src/main/java/com/icogroup/dallassuites/Restaurant.java
@@ -1,24 +1,33 @@
 package com.icogroup.dallassuites;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
  * Created by andres.torres on 10/29/14.
  */
-public class Restaurant extends Activity implements View.OnClickListener {
+public class Restaurant extends Activity {
 
-    ImageButton back;
-    TextView title, desayuno, ensaladas, depicar, sandwiches, pizzas, postres, restodeldia, bebidas;
-    Button bDesayuno, bEnsaladas, bDePicar, bSandwiches, bPizzas, bPostres, bRestoDelDia, bBebidas;
-    Typeface brandonregular, brandonlight;
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableAdapter;
+    String[] categorias = {"Desayuno", "Ensaladas", "De picar", "Sandwiches", "Pizzas", "Parrillas", "Resto del día", "A la plancha", "Snacks 24 horas", "Bebidas", "Postres" };
+    String[] bebidas = {"Champagne y espumantes", "Vinos", "Whiskies", "Rones", "Vodka", "Gyn", "Aperitivos y tragos preparados", "Cocktails"};
+    String[] keywordsCategorias = {"desayuno", "plancha", "resto", "parrilla", "ensalada", "picar", "sandwich", "pizza", "postre", "snack"};
+    String[] keywordsBebidas = {"champagne", "espumante", "vino", "whisky", "ron", "vodka", "gyn", "aperitivo", "coctel", "otras", "batido", "café"};
+    int[] imagesCategorias = {R.drawable.iconos_desayuno, R.drawable.iconos_ensalada, R.drawable.iconos_depicar, R.drawable.iconos_sandwiches, R.drawable.iconos_pizza, R.drawable.iconos_alaplancha,R.drawable.iconos_restodeldia, R.drawable.iconos_alaplancha, R.drawable.iconos_snaks, R.drawable.iconos_bebidas, R.drawable.iconos_postres};
 
+    Typeface brandonreg;
+    TextView tvCategoria, title, tvBebidas;
+    ImageView ivCategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,74 +35,139 @@ public class Restaurant extends Activity implements View.OnClickListener {
         setContentView(R.layout.restaurant);
 
         init();
+
+        expandableListView.setAdapter(expandableAdapter);
+
+        expandableListView.setGroupIndicator(null);
+
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long l) {
+
+           
+
+                return false;
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i2, long l) {
+
+                return false;
+            }
+        });
+
     }
 
     private void init() {
 
-        brandonregular = Typeface.createFromAsset(getAssets(), "brandon_reg.otf");
-        brandonlight = Typeface.createFromAsset(getAssets(), "brandon_light.otf");
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
 
-        title = (TextView) findViewById(R.id.restaurant_title);
+        expandableAdapter = new ExpandableListAdapter(getApplicationContext());
 
-        back = (ImageButton)findViewById(R.id.restaurant_back);
+        brandonreg = Typeface.createFromAsset(getAssets(), "brandon_reg.otf");
 
-        desayuno = (TextView) findViewById(R.id.restaurant_text_desayuno);
-        ensaladas = (TextView) findViewById(R.id.restaurant_text_ensaladas);
-        depicar = (TextView) findViewById(R.id.restaurant_text_depicar);
-        sandwiches = (TextView) findViewById(R.id.restaurant_text_sandwiches);
-        pizzas = (TextView) findViewById(R.id.restaurant_text_pizzas);
-        postres = (TextView) findViewById(R.id.restaurant_text_postres);
-        restodeldia = (TextView) findViewById(R.id.restaurant_text_restodeldia);
-        bebidas = (TextView) findViewById(R.id.restaurant_text_bebidas);
-
-        bDesayuno = (Button) findViewById(R.id.restaurant_button_desayuno);
-        bEnsaladas = (Button) findViewById(R.id.restaurant_button_ensaladas);
-        bDePicar = (Button) findViewById(R.id.restaurant_button_depicar);
-        bSandwiches = (Button) findViewById(R.id.restaurant_button_sandwiches);
-        bPizzas = (Button) findViewById(R.id.restaurant_button_pizzas);
-        bPostres = (Button) findViewById(R.id.restaurant_button_postres);
-        bRestoDelDia = (Button) findViewById(R.id.restaurant_button_restodeldia);
-        bBebidas = (Button) findViewById(R.id.restaurant_button_bebidas);
-
-        back.setOnClickListener(this);
-
-        bDesayuno.setOnClickListener(this);
-        bEnsaladas.setOnClickListener(this);
-        bDePicar.setOnClickListener(this);
-        bSandwiches.setOnClickListener(this);
-        bPizzas.setOnClickListener(this);
-        bPostres.setOnClickListener(this);
-        bRestoDelDia.setOnClickListener(this);
-        bBebidas.setOnClickListener(this);
+        title = (TextView)findViewById(R.id.restaurant_title);
+        title.setTypeface(brandonreg);
 
 
-        title.setTypeface(brandonlight);
-
-        desayuno.setTypeface(brandonregular);
-        ensaladas.setTypeface(brandonregular);
-        depicar.setTypeface(brandonregular);
-        sandwiches.setTypeface(brandonregular);
-        pizzas.setTypeface(brandonregular);
-        postres.setTypeface(brandonregular);
-        restodeldia.setTypeface(brandonregular);
-        bebidas.setTypeface(brandonregular);
 
     }
 
-    @Override
-    public void onClick(View view) {
 
-        switch(view.getId()){
+    class ExpandableListAdapter extends BaseExpandableListAdapter {
 
-            case R.id.restaurant_back:
-                finish();
-                break;
+        private Context _context;
 
-            default:
-                startActivity(new Intent(Restaurant.this, RestaurantDetail.class));
-                break;
-
+        public ExpandableListAdapter(Context context) {
+            this._context = context;
         }
 
+        @Override
+        public int getGroupCount() {
+            return categorias.length;
+        }
+
+        @Override
+        public int getChildrenCount(int i) {
+
+            if(categorias[i].contains("Bebidas"))
+                return bebidas.length;
+            else
+                return 0;
+        }
+
+        @Override
+        public Object getGroup(int i) {
+            return categorias[i];
+        }
+
+        @Override
+        public Object getChild(int i, int i2) {
+            return bebidas[i];
+        }
+
+        @Override
+        public long getGroupId(int i) {
+            return i;
+        }
+
+        @Override
+        public long getChildId(int i, int i2) {
+            return i;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+
+
+            if (convertView == null) {
+                LayoutInflater infalInflater = (LayoutInflater) this._context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = infalInflater.inflate(R.layout.categorias, null);
+            }
+
+            tvCategoria = (TextView)convertView.findViewById(R.id.text_categoria);
+            tvCategoria.setText(categorias[groupPosition]);
+            tvCategoria.setTypeface(brandonreg);
+
+            ivCategoria = (ImageView)convertView.findViewById(R.id.image_categoria);
+            ivCategoria.setImageResource(imagesCategorias[groupPosition]);
+
+            return convertView;
+        }
+
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater infalInflater = (LayoutInflater) this._context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = infalInflater.inflate(R.layout.bebidas, null);
+
+                tvBebidas = (TextView)convertView.findViewById(R.id.text_bebida);
+                tvBebidas.setText(bebidas[childPosition]);
+                tvBebidas.setTypeface(brandonreg);
+
+            }
+
+
+            return convertView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int i, int i2) {
+            return false;
+        }
     }
+
+
+
+
+
 }
